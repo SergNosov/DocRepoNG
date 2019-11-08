343eb52dc4356f6ab2f66a0f8cfcc63c833e8aed
Serg N <serg_vn@inbox.ru>
2019-11-08 15:55:03 +0900

    refractoring deleteSender in sender-rest-table

--- src/app/views/sender-rest-table/sender-rest-table.component.ts
+++ src/app/views/sender-rest-table/sender-rest-table.component.ts
@@ -1,46 +1,52 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {Sender} from "../../model/Sender";
 import {SenderServiceRest} from "../../service_rest/senderRest.service";
 import {CommonMessage} from "../../model/common-message";
 import {Observable} from "rxjs";
 
 @Component({
-  selector: 'app-sender-rest-table',
-  templateUrl: './sender-rest-table.component.html',
-  styleUrls: ['./sender-rest-table.component.css']
+    selector: 'app-sender-rest-table',
+    templateUrl: './sender-rest-table.component.html',
+    styleUrls: ['./sender-rest-table.component.css']
 })
 export class SenderRestTableComponent implements OnInit {
 
-  private senders: Observable<Sender[]>;
-  private commonMessage: Observable<CommonMessage>;
+    private senders: Observable<Sender[]>;
+    private commonMessage: Observable<CommonMessage>;
 
-  constructor(private senderServiceRest: SenderServiceRest) { }
-
-  ngOnInit() {
-    this.reloadData();
-    /*
-    this.senderServiceRest.getAllSenders()
-        .subscribe(data => {this.senders = data})
-     */
-  }
-
-  public newSender() {
-  }
-
-  public editSender(sender: Sender){
-  }
-
-  public deleteSender(sender: Sender) {
-    if(confirm("Удалить отправителя: id="+sender.id+"; "+sender.title+"?")) {
-      this.senderServiceRest.delete(sender).subscribe(data => {
-       console.log(data.valueOf());
-       this.reloadData();
-       alert(data.message);
-      });
+    constructor(private senderServiceRest: SenderServiceRest) {
     }
-  }
 
-  private reloadData(){
-    this.senders = this.senderServiceRest.getAllSenders();
-  }
+    ngOnInit() {
+        this.reloadData();
+        /*
+        this.senderServiceRest.getAllSenders()
+            .subscribe(data => {this.senders = data})
+         */
+    }
+
+    public newSender() {
+    }
+
+    public editSender(sender: Sender) {
+    }
+
+    public deleteSender(sender: Sender) {
+        if (confirm("Удалить отправителя: id = " + sender.id + "; " + sender.title + "?")) {
+            this.senderServiceRest.delete(sender).subscribe(
+                data => {
+                    console.log(data.valueOf());
+                    this.reloadData();
+                    alert(data.message);
+                },
+                error => {
+                    console.log(error.valueOf());
+                    alert("Невозможно удалить отправителя: id = " + sender.id + "\n" + error.message);
+                });
+        }
+    }
+
+    private reloadData() {
+        this.senders = this.senderServiceRest.getAllSenders();
+    }
 }
